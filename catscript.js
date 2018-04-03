798731d8a890ac499d45bb72f9d305efade3bb18
unknown <robin.ramos@dataseer.com>
2018-04-03 19:43:00 +0800

    added event pagination and previous button

--- catscript.js
+++ catscript.js
@@ -34,15 +34,15 @@ var tooltip = d3.select("#post").append("div")
     .style("opacity", 0);
 
 var milestones = [
-  {date: parseTime("2015-05-30"), event: "First post in Instagram! The Cats of BGC has been known since 2015."},
+  {date: parseTime("2015-05-30"), event: "First post in Instagram! The Cats of BGC has been known since 2015.  <div class='iframe-container'><blockquote class='instagram-media' data-instgrm-captioned data-instgrm-permalink='https://www.instagram.com/p/3ShN1uScI_/' data-instgrm-version='8' style=' background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);'><div style='padding:8px;'> <div style=' background:#F8F8F8; line-height:0; margin-top:40px; padding:50.0% 0; text-align:center; width:100%;'> <div style=' background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;'></div></div> <p style=' margin:8px 0 0 0; padding:0 4px;'> <a href='https://www.instagram.com/p/3ShN1uScI_/' style=' color:#000; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none; word-wrap:break-word;' target='_blank'>Meow #catsofbgc</a></p> <p style=' color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;'>A post shared by <a href='https://www.instagram.com/thisisagu/' style=' color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px;' target='_blank'> agu</a> (@thisisagu) on <time style=' font-family:Arial,sans-serif; font-size:14px; line-height:17px;' datetime='2015-05-30T02:30:17+00:00'>May 29, 2015 at 7:30pm PDT</time></p></div></blockquote> </div>"},
   {date: parseTime("2016-08-04"), event: "More people posted about the cats in 2016..."},
   {date: parseTime("2017-08-28"), event: "...and in 2017."},
   {date: parseTime("2018-02-16"), event: "A Facebook post by Lucy M went viral. The post with 4.8K and 12K shares tells the story about how Shangri-La BGC ordered Pestbusters to remove the cats from their perimeter. <br> <br> <div class='iframe-container'> <iframe src='https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fmarcellejohn.marcelino%2Fposts%2F10216653759442825&width=350&show_text=true&appId=1488368734536934&height=553' width='350' height='553' style='border:none;overflow:hidden' scrolling='yes' frameborder='0' allowTransparency='true'></iframe></div>"},
-    {date: parseTime("2018-02-18"), event: "Shangri-La BGC posted a statement on their Facebook account. <br><br> <div class='iframe-container'> <iframe src='https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fshangrilafort%2Fphotos%2Fa.996102000419770.1073741828.994986307198006%2F1998901190139841%2F%3Ftype%3D3&width=350&show_text=true&height=452&appId' width='350' height='452' style='border:none;overflow:hidden' scrolling='no' frameborder='0' allowTransparency='true'></iframe></div>"},
+  {date: parseTime("2018-02-18"), event: "Shangri-La BGC posted a statement on their Facebook account. <br><br> <div class='iframe-container'> <iframe src='https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fshangrilafort%2Fphotos%2Fa.996102000419770.1073741828.994986307198006%2F1998901190139841%2F%3Ftype%3D3&width=350&show_text=true&height=452&appId' width='350' height='452' style='border:none;overflow:hidden' scrolling='no' frameborder='0' allowTransparency='true'></iframe></div>"},
   {date: parseTime("2018-02-19"), event: "Inquirer.net posted an <a href='http://newsinfo.inquirer.net/969674/loss-of-taguig-park-cats-sparks-outcry' target='_blank'>article</a> about the issue."},
   {date: parseTime("2018-03-01"), event: "Event 7"}];
 
-var nextMilestone = 0;
+var nextMilestone = -1;
 var transitionDuration = 2000;
 var dataMilestone = {}; 
 var dataMilestoneScatter = {};
@@ -265,9 +265,35 @@ d3.csv("allposts.csv", function(error, data) {
    var x1 = d3.scaleLinear().range([0, 90]);
    var y1 = d3.scaleBand().range([0, 100]);
 
-function nextStep() {
+function nextButton() {
+  nextMilestone = nextMilestone + 1;
+ 
+  if (nextMilestone == 0) {
+    document.getElementById("milestone-btn2").disabled = false;
+    document.getElementById("step-" + (nextMilestone+1)).classList.add("stepactive");
+  }
+  else if (nextMilestone == 7){
+    document.getElementById("step-" + (nextMilestone)).classList.remove("stepactive");
+  }
+  else {
+    document.getElementById("step-" + (nextMilestone+1)).classList.add("stepactive");
+    document.getElementById("step-" + (nextMilestone)).classList.remove("stepactive");
+  }
+  nextStep(nextMilestone)
+}
 
-  if (milestones.length == nextMilestone) {
+function prevButton() {
+  nextMilestone = nextMilestone - 1;
+  nextStep(nextMilestone)
+  if (nextMilestone == 0) {
+    document.getElementById("milestone-btn2").disabled = true;
+  }
+}
+
+function nextStep(nextMile) {
+console.log(nextMile)
+
+  if (milestones.length == nextMile) {
     svg2.selectAll("#clip-sentiment").selectAll("rect")
       .transition().duration(2000)
       .attr("width",0);
@@ -291,8 +317,8 @@ function nextStep() {
     for (var i = milestonepoints.length - 1; i >= 0; i--) {
       milestonepoints[i].remove();
     }
-
-    nextMilestone = 0;
+    document.getElementById("milestone-btn2").disabled = true;
+    nextMilestone = -1;
     svg2.selectAll(".circle-label").remove();
     document.getElementById("milestone-btn").innerHTML ="Next";}
 
@@ -302,14 +328,14 @@ function nextStep() {
 
     svg2.selectAll("#clip-sentiment").selectAll("rect")
       .transition().duration(2000)
-      .attr("width", x(milestones[nextMilestone].date) );
+      .attr("width", x(milestones[nextMile].date) );
 
     svg.selectAll("#clip-popularity").selectAll("rect")
       .transition().duration(2000)
-      .attr("width", x(milestones[nextMilestone].date) );
+      .attr("width", x(milestones[nextMile].date) );
 
     svg3.selectAll("circle")
-      .filter(function(d){return d.time <= milestones[nextMilestone].date})
+      .filter(function(d){return d.time <= milestones[nextMile].date})
       .transition()
       .duration(2000)
       .style("fill-opacity", 0.5)
@@ -319,14 +345,14 @@ function nextStep() {
       .data(dataMilestone)
       .enter()
       .filter(function (d) { 
-        return d.time.toDateString() == milestones[nextMilestone].date.toDateString() ;
+        return d.time.toDateString() == milestones[nextMile].date.toDateString() ;
       });
 
     var milestonePoint2 = svg.selectAll(".milestone-point")
       .data(dataMilestone)
       .enter()
       .filter(function (d) { 
-        return d.time.toDateString() == milestones[nextMilestone].date.toDateString() ;
+        return d.time.toDateString() == milestones[nextMile].date.toDateString() ;
       });
 
 
@@ -346,7 +372,7 @@ function nextStep() {
       .attr("transform", function (d) { return "translate(" + x(d.time) + ",0)" ; } );
 
     milestonePoint.append("circle")
-      .attr("id", function (d) { return "circle-milestone-" + nextMilestone ; })
+      .attr("id", function (d) { return "circle-milestone-" + nextMile ; })
       .attr("class", "milestone-circle")
       .attr("cx", function (d) { return x(d.time) } )
       .attr("cy", function (d) { return y2(d.score) } )
@@ -355,7 +381,7 @@ function nextStep() {
       .style("stroke-opacity", 0);
 
     milestonePoint2.append("circle")
-      .attr("id", function (d) { return "circle-milestone2-" + nextMilestone ; })
+      .attr("id", function (d) { return "circle-milestone2-" + nextMile ; })
       .attr("class", "milestone-circle")
       .attr("cx", function (d) { return x(d.time) } )
       .attr("cy", function (d) { return y(d.count) } )
@@ -371,24 +397,24 @@ function nextStep() {
       ;
 
 
-    milestonePoint.selectAll("#circle-milestone-" + nextMilestone)
+    milestonePoint.selectAll("#circle-milestone-" + nextMile)
       .transition()
       .duration(2000)
       .style("stroke-opacity", 0.9)
       .style("fill-opacity", 0.9);
 
-    milestonePoint2.selectAll("#circle-milestone2-" + nextMilestone)
+    milestonePoint2.selectAll("#circle-milestone2-" + nextMile)
       .transition()
       .duration(2000)
       .style("stroke-opacity", 0.9)
       .style("fill-opacity", 0.9);
 
-      document.getElementById("intro-text").innerHTML = '<br>' + milestones[nextMilestone].event;
+      document.getElementById("intro-text").innerHTML = '<br>' + milestones[nextMile].event;
+      window.instgrm.Embeds.process();
 
-    nextMilestone = nextMilestone + 1;
-
-    if (milestones.length == nextMilestone) {
+    if (milestones.length == nextMile+1) {
       document.getElementById("milestone-btn").innerHTML ="Reset";
+
     }}}
 
 
@@ -428,6 +454,5 @@ d3.csv("keywords.csv", function(error,data) {
         .attr("height", y1.bandwidth())
         .attr("y", function(d) { return y1(d.word); })
         .attr("width", function(d) { return x1(d.freq); })
-})
-
+  })
 }
