352db34bf730195c5a18b144705f331a1d3f1082
unknown <robin.ramos@dataseer.com>
2018-04-04 04:01:10 +0800

    changed layout. added tooltips

--- catscript.js
+++ catscript.js
@@ -8,7 +8,9 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
   
 var parseTime = d3.timeParse("%Y-%m-%d");
 var formatTime = d3.timeFormat("%d/%m/%Y");
-
+var formatDate = d3.timeFormat("%B %d, %Y");
+var formatComma = d3.format(",");
+var formatDecimal = d3.format(".2f");
 
 var x = d3.scaleTime().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
@@ -34,13 +36,15 @@ var tooltip = d3.select("#post").append("div")
     .style("opacity", 0);
 
 var milestones = [
-  {date: parseTime("2015-05-30"), event: "First post in Instagram! The Cats of BGC has been known since 2015.  <div class='iframe-container'><blockquote class='instagram-media' data-instgrm-captioned data-instgrm-permalink='https://www.instagram.com/p/3ShN1uScI_/' data-instgrm-version='8' style=' background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);'><div style='padding:8px;'> <div style=' background:#F8F8F8; line-height:0; margin-top:40px; padding:50.0% 0; text-align:center; width:100%;'> <div style=' background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;'></div></div> <p style=' margin:8px 0 0 0; padding:0 4px;'> <a href='https://www.instagram.com/p/3ShN1uScI_/' style=' color:#000; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none; word-wrap:break-word;' target='_blank'>Meow #catsofbgc</a></p> <p style=' color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;'>A post shared by <a href='https://www.instagram.com/thisisagu/' style=' color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px;' target='_blank'> agu</a> (@thisisagu) on <time style=' font-family:Arial,sans-serif; font-size:14px; line-height:17px;' datetime='2015-05-30T02:30:17+00:00'>May 29, 2015 at 7:30pm PDT</time></p></div></blockquote> </div>"},
-  {date: parseTime("2016-08-04"), event: "More people posted about the cats in 2016..."},
-  {date: parseTime("2017-08-28"), event: "...and in 2017."},
+  {date: parseTime("2014-05-12"), event: "First tweet! People have been started tweeting about Cats of BGC since 2014.  <div class='iframe-container'><blockquote class='twitter-tweet' data-lang='en'><p lang='en' dir='ltr'>Some idiot put handcuffs on the neck of this poor cat. Seen moments ago at BGC 7th cor 32nd near the bus stop. <a href='https://twitter.com/hashtag/PAWS?src=hash&amp;ref_src=twsrc%5Etfw'>#PAWS</a> <a href='http://t.co/oXoAA7irNy'>pic.twitter.com/oXoAA7irNy</a></p>&mdash; TJS Daily (@TJSDaily) <a href='https://twitter.com/TJSDaily/status/465790905224404992?ref_src=twsrc%5Etfw'>May 12, 2014</a></blockquote><script async src='https://platform.twitter.com/widgets.js' charset='utf-8'></script> </div>"},
+  {date: parseTime("2016-11-17"), event: "More people posted about the cats in 2016..."},
+  {date: parseTime("2017-12-31"), event: "...and in 2017."},
   {date: parseTime("2018-02-16"), event: "A Facebook post by Lucy M went viral. The post with 4.8K and 12K shares tells the story about how Shangri-La BGC ordered Pestbusters to remove the cats from their perimeter. <br> <br> <div class='iframe-container'> <iframe src='https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fmarcellejohn.marcelino%2Fposts%2F10216653759442825&width=350&show_text=true&appId=1488368734536934&height=553' width='350' height='553' style='border:none;overflow:hidden' scrolling='yes' frameborder='0' allowTransparency='true'></iframe></div>"},
   {date: parseTime("2018-02-18"), event: "Shangri-La BGC posted a statement on their Facebook account. <br><br> <div class='iframe-container'> <iframe src='https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fshangrilafort%2Fphotos%2Fa.996102000419770.1073741828.994986307198006%2F1998901190139841%2F%3Ftype%3D3&width=350&show_text=true&height=452&appId' width='350' height='452' style='border:none;overflow:hidden' scrolling='no' frameborder='0' allowTransparency='true'></iframe></div>"},
   {date: parseTime("2018-02-19"), event: "Inquirer.net posted an <a href='http://newsinfo.inquirer.net/969674/loss-of-taguig-park-cats-sparks-outcry' target='_blank'>article</a> about the issue."},
-  {date: parseTime("2018-03-31"), event: "Event 7"}];
+  {date: parseTime("2018-02-20"), event: "Shangri-La BGC's manager posted a public apology on their Facebook account <br> <iframe src='https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fweb.facebook.com%2Fshangrilafort%2Fposts%2F2001217123241581%3A0&width=350&show_text=true&appId=1488368734536934&height=144' width='350' height='144' style='border:none;overflow:hidden' scrolling='no' frameborder='0' allowTransparency='true'></iframe>"},
+  {date: parseTime("2018-02-23"), event: "After being silent for a few days, Pestbusters has finally voiced out their side. <br> <div class='iframe-container'><iframe src='https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fweb.facebook.com%2Fzeropests%2Fposts%2F899416996900761&width=350&show_text=true&appId=1488368734536934&height=541' width='350' height='541' style='border:none;overflow:hidden' scrolling='no' frameborder='0' allowTransparency='true'></iframe></div>"},
+  {date: parseTime("2018-03-31"), event: "Until today, netizens does not seem to forget about the incident."}];
 
 var nextMilestone = -1;
 var transitionDuration = 2000;
@@ -99,6 +103,8 @@ d3.csv("catsofbgc.csv", function(error, data) {
   x.domain(d3.extent(data, function(d) { return d.time; }));
   y.domain([0, d3.max(data, function(d) { return d.count; })]);
 
+
+
   svg.append("clipPath")
     .attr("id", "clip-popularity")
     .append("rect")
@@ -116,10 +122,24 @@ d3.csv("catsofbgc.csv", function(error, data) {
 
   svg.append("g")
       .attr("transform", "translate(0," + height + ")")
+      .attr("class", "axisLine")
       .call(d3.axisBottom(x).tickSizeOuter(0));
 
   svg.append("g")
+      .attr("class", "axisLine")
       .call(d3.axisLeft(y).tickSizeOuter(0));
+
+  svg.append("text")
+      .attr("class", "axisLine")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 10 - margin.left)
+      .attr("x",0 - (height / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .style("fill", "#A2C2B9")
+      .style("font-size", "10px")
+      .text("Number of posts");
+
 });
 
 // Sentiment Line Chart (svg2) ----------------
@@ -162,8 +182,8 @@ d3.csv("catsofbgc.csv", function(error, data) {
     .data([
       {offset: "0%", color: "#737A81"},
       {offset: "50%", color: "#737A81"},
-      {offset: "50%", color: "#F1948A"},
-      {offset: "100%", color: "#F1948A"}
+      {offset: "50%", color: "#FD1772"},
+      {offset: "100%", color: "#FD1772"}
     ])
     .enter().append("stop")
     .attr("offset", function(d) { return d.offset; })
@@ -183,10 +203,20 @@ d3.csv("catsofbgc.csv", function(error, data) {
       .attr("d", sentimentline);
 
   svg2.append("g")
+      .attr("class", "axisLine")
       .attr("transform", "translate(0," + height2/2 + ")")
       .call(d3.axisBottom(x).tickSizeOuter(0));
 
-  svg2.append("g").call(d3.axisLeft(y2).tickSizeOuter(0));
+  svg2.append("text")
+      .attr("class", "axisLine")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left/2)
+      .attr("x",0 - (height2 / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .style("fill", "#A2C2B9")
+      .style("font-size", "10px")
+      .text("-  Sentiment  +"); 
 
   svg2.selectAll(".tick")
       .each(function (d) {
@@ -211,7 +241,9 @@ d3.csv("allposts.csv", function(error, data) {
       d.link = d.link;
       d.platform = d.platform;
       d.sentiment = +d.score;
-    robin = d.time;})
+      d.followers =  Math.pow(Math.log10(+d.followers_count+1),2);
+      d.followers_count = +d.followers_count;
+      })
       
 
   dataMilestoneScatter = data;
@@ -219,20 +251,50 @@ d3.csv("allposts.csv", function(error, data) {
   x.domain(d3.extent(data, function(d) { return d.time; }));
   y3.domain([0, d3.max(data, function(d) { return d.popularity; })]);
 
+  svg3.append("g")
+      .attr("class", "axisLine")
+      .attr("transform", "translate(0," + height3 + ")")
+      .call(d3.axisBottom(x).tickSizeOuter(0));
+
+  svg3.append("g").attr("class", "axisLine").call(d3.axisLeft(y3));
+
+  svg3.append("text")
+      .attr("class", "axisLine")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x",0 - (height3 / 2))
+      .attr("dy", "1em")
+      .style("text-anchor", "middle")
+      .style("fill", "#A2C2B9")
+      .style("font-size", "14px")
+      .text("Popularity score");
+
+  var tip2 = d3.tip()
+      .attr("class", "d3-tip")
+      .offset([-10, 0])
+      .html(function(d)
+      {
+        return "A tweet posted on <b>" + formatDate(d.time) + "</b><br>"
+        + "by a user with <b>" + formatComma(d.followers_count) + "</b> followers." ;
+      });
+
+  svg3.call(tip2)
 
   svg3.selectAll("dot")
     .data(dataMilestoneScatter)
     .enter().append("circle")
     .filter(function(d) {return d.time})
     .attr("class", "scatterplots")
-    .attr("r", 3)
+    .attr("r", function(d) { return d.followers})
     .attr("cx", function(d) { return x(d.time); })
     .attr("cy", function(d) { return y3(d.popularity); })
     .attr("fill-opacity", 0)
-    .style("fill", function(d){ if (d.sentiment < 0) {return 'red'} else {return 'black'}})
+    .style("fill", function(d){ if (d.sentiment < 0) {return '#FD1772'} else {return '#737A81'}})
     .on("mouseover", function(d) {   
-        d3.select(this).transition().attr('r',8);
+        d3.select(this).transition().style('fill', '#1F2929');
+
         if (this.getAttribute("value") == "show") {
+          tip2.show(d);
           d3.select(this).style("cursor","pointer")
             reload();
             loadTweet2(d.id);
@@ -240,8 +302,9 @@ d3.csv("allposts.csv", function(error, data) {
         else { //if value is hide       
         }})          
     .on("mouseout", function(d) {  
-        d3.select(this).transition().attr('r',3);
+      d3.select(this).transition().style("fill", function(d){ if (d.sentiment < 0) {return '#FD1772'} else {return '#737A81'}});
         if (this.getAttribute("value") == "show") {
+          tip2.hide();
           d3.select(this).style("cursor","default")
           if (d.platform == "tw") {
             //reload();
@@ -250,11 +313,7 @@ d3.csv("allposts.csv", function(error, data) {
             //reload();
           }}});
 
-  svg3.append("g")
-      .attr("transform", "translate(0," + height3 + ")")
-      .call(d3.axisBottom(x).tickSizeOuter(0));
 
-  svg3.append("g").call(d3.axisLeft(y3));
 });
 
 // Data story
@@ -266,10 +325,14 @@ function nextButton() {
   nextMilestone = nextMilestone + 1;
  
   if (nextMilestone == 0) {
-    document.getElementById("milestone-btn2").disabled = false;
     document.getElementById("step-" + (nextMilestone+1)).classList.add("stepactive");
   }
-  else if (nextMilestone == 7){
+  else if (nextMilestone == 1) {
+    document.getElementById("milestone-btn2").disabled = false;
+    document.getElementById("step-" + (nextMilestone+1)).classList.add("stepactive");
+    document.getElementById("step-" + (nextMilestone)).classList.remove("stepactive");
+  }
+  else if (nextMilestone == 9){
     document.getElementById("step-" + (nextMilestone)).classList.remove("stepactive");
   }
   else {
@@ -280,15 +343,18 @@ function nextButton() {
 }
 
 function prevButton() {
+  document.getElementById("step-" + (nextMilestone)).classList.add("stepactive");
+  document.getElementById("step-" + (nextMilestone+1)).classList.remove("stepactive");
   nextMilestone = nextMilestone - 1;
-  nextStep(nextMilestone)
-  if (nextMilestone == 0) {
+  console.log(nextMilestone)
+  if (nextMilestone < 1) {
     document.getElementById("milestone-btn2").disabled = true;
   }
+
+  nextStep(nextMilestone)
 }
 
 function nextStep(nextMile) {
-console.log(nextMile)
 
   if (milestones.length == nextMile) {
     svg2.selectAll("#clip-sentiment").selectAll("rect")
@@ -327,6 +393,10 @@ console.log(nextMile)
       .transition().duration(2000)
       .attr("width", x(milestones[nextMile].date) );
 
+    svg2.selectAll("#clip-sentiment").selectAll("rect")
+      .transition().duration(2000)
+      .attr("width", x(milestones[nextMile].date) );
+
     svg.selectAll("#clip-popularity").selectAll("rect")
       .transition().duration(2000)
       .attr("width", x(milestones[nextMile].date) );
@@ -342,6 +412,8 @@ console.log(nextMile)
       .data(dataMilestone)
       .enter()
       .filter(function (d) { 
+        console.log(milestones[nextMile].date.toDateString())
+        console.log(d.time.toDateString())
         return d.time.toDateString() == milestones[nextMile].date.toDateString() ;
       });
 
@@ -355,14 +427,23 @@ console.log(nextMile)
 
    var tip = d3.tip()
       .attr("class", "d3-tip")
-      .offset([-100, 0])
+      .offset([-10, 0])
       .html(function(d)
       {
-        return "Date: " + formatTime(d.time) + "<br>"
-        + "<div id='tipDiv'></div>" ;
+        return "Total number of posts on  <b>" + formatDate(d.time) + "</b>: <b>" + d.count + "</b>";
       });
 
     svg.call(tip)
+
+   var tip3 = d3.tip()
+      .attr("class", "d3-tip")
+      .offset([-10, 0])
+      .html(function(d)
+      {
+        return "Sentiment score on  <b>" + formatDate(d.time) + "</b>: <b>" + formatDecimal(d.score) + "</b>";
+      });
+
+    svg2.call(tip3)
 
     milestonePoint.append("g")
       .attr("class", "milestone-point")
@@ -373,22 +454,27 @@ console.log(nextMile)
       .attr("class", "milestone-circle")
       .attr("cx", function (d) { return x(d.time) } )
       .attr("cy", function (d) { return y2(d.score) } )
-      .attr("r", 2)
+      .attr("r", 5)
       .style("fill-opacity", 0)        
-      .style("stroke-opacity", 0);
+      .style("stroke-opacity", 0)
+      .on("mouseover", function(d) {
+          tip3.show(d);  
+      })
+      .on("mouseout", tip3.hide);
+
 
     milestonePoint2.append("circle")
       .attr("id", function (d) { return "circle-milestone2-" + nextMile ; })
       .attr("class", "milestone-circle")
       .attr("cx", function (d) { return x(d.time) } )
       .attr("cy", function (d) { return y(d.count) } )
-      .attr("r", 2)
+      .attr("r", 5)
       .style("fill-opacity", 0)        
       .style("stroke-opacity", 0)   
       .style("cursor", "pointer")   
       .on("mouseover", function(d) {
           tip.show(d);  
-          keytip(d.time)
+          //keytip(d.time)
       })
       .on("mouseout", tip.hide )
       ;
@@ -406,8 +492,8 @@ console.log(nextMile)
       .style("stroke-opacity", 0.9)
       .style("fill-opacity", 0.9);
 
-      document.getElementById("intro-text").innerHTML = '<br>' + milestones[nextMile].event;
-      window.instgrm.Embeds.process();
+      document.getElementById("intro-text").innerHTML = formatDate(milestones[nextMile].date) + '<br>' + milestones[nextMile].event;
+      window.twttr.widgets.load();
 
     if (milestones.length == nextMile+1) {
       document.getElementById("milestone-btn").innerHTML ="Reset";
